9f8c244229c9bfd423d787ab9daf0efb6e23a94c
mmirwaldt <michael.mirwaldt@gmail.com>
2021-05-24 04:24:25 +0200

    renamed parameter name

--- src/main/java/net/mirwaldt/aoc/year2015/day25/TailCallRecursionDiagonalCodeGenerator.java
+++ src/main/java/net/mirwaldt/aoc/year2015/day25/TailCallRecursionDiagonalCodeGenerator.java
@@ -8,15 +8,15 @@ public class TailCallRecursionDiagonalCodeGenerator implements DiagonalCodeGener
         return recursiveGenerateCode(row, col, startValue, operator).invoke();
     }
 
-    private TailCall<Long> recursiveGenerateCode(int row, int col, long value, LongUnaryOperator operator) {
+    private TailCall<Long> recursiveGenerateCode(int row, int col, long result, LongUnaryOperator operator) {
         if (row == 1 && col == 1) {
-            return TailCalls.done(value);
+            return TailCalls.done(result);
         } else {
             return () -> {
                 if (col == 1) {
-                    return recursiveGenerateCode(1, row - 1, operator.applyAsLong(value), operator);
+                    return recursiveGenerateCode(1, row - 1, operator.applyAsLong(result), operator);
                 } else {
-                    return recursiveGenerateCode(row + 1, col - 1, operator.applyAsLong(value), operator);
+                    return recursiveGenerateCode(row + 1, col - 1, operator.applyAsLong(result), operator);
                 }
             };
         }
