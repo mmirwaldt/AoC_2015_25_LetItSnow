1fdc751324dbf9d0a0158d1cef063947f86d8edf
mmirwaldt <michael.mirwaldt@gmail.com>
2021-05-24 04:45:00 +0200

    added recursive methods in which the calculated result corresponds with the current row and col value in the parameter list

--- src/main/java/net/mirwaldt/aoc/year2015/day25/RecursiveDiagonalCodeGenerator.java
+++ src/main/java/net/mirwaldt/aoc/year2015/day25/RecursiveDiagonalCodeGenerator.java
@@ -10,20 +10,33 @@ import java.util.function.LongUnaryOperator;
 public class RecursiveDiagonalCodeGenerator implements DiagonalCodeGenerator {
     @Override
     public long generateCode(int row, int col, long startValue, LongUnaryOperator operator) {
-        if(row < 1 || col < 1) {
+        if (row < 1 || col < 1) {
             throw new IllegalArgumentException("Row and col must be at least 1 and not row=" + row + ", col=" + col);
         } else {
-            return recursiveGenerateCode(row, col, startValue, operator);
+            return recursiveGenerateCode2(row, col, 1, 1, startValue, operator);
         }
     }
 
     private long recursiveGenerateCode(int row, int col, long startValue, LongUnaryOperator operator) {
-        if(row == 1 && col==1) {
+        if (row == 1 && col == 1) {
             return startValue;
-        } else if(col == 1) {
+        } else if (col == 1) {
             return operator.applyAsLong(recursiveGenerateCode(1, row - 1, startValue, operator));
         } else {
             return operator.applyAsLong(recursiveGenerateCode(row + 1, col - 1, startValue, operator));
+        }
+    }
+
+    private long recursiveGenerateCode2(
+            int row, int col, int curRow, int curCol, long startValue, LongUnaryOperator operator) {
+        if (row == curRow && col == curCol) {
+            return startValue;
+        } else if (curRow == 1) {
+            return operator.applyAsLong(recursiveGenerateCode2(
+                    row, col, curCol + 1, 1, startValue, operator));
+        } else {
+            return operator.applyAsLong(recursiveGenerateCode2(
+                    row, col, curRow - 1, curCol + 1, startValue, operator));
         }
     }
 }
--- src/main/java/net/mirwaldt/aoc/year2015/day25/TailCallRecursionDiagonalCodeGenerator.java
+++ src/main/java/net/mirwaldt/aoc/year2015/day25/TailCallRecursionDiagonalCodeGenerator.java
@@ -5,9 +5,17 @@ import java.util.function.LongUnaryOperator;
 public class TailCallRecursionDiagonalCodeGenerator implements DiagonalCodeGenerator {
     @Override
     public long generateCode(int row, int col, long startValue, LongUnaryOperator operator) {
-        return recursiveGenerateCode(row, col, startValue, operator).invoke();
+        return recursiveGenerateCode2(row, col, 1, 1, startValue, operator).invoke();
     }
 
+    /*
+     * The tail call optimization makes row and col count down to 1,1
+     * but starts from the start value to calculate the result.
+     * Therefore don't confuse the values of row and cols on the one side and the result on the side:
+     * recursiveGenerateCode(x, y, result, ...) doesn't imply
+     * that the result is the value for cell with row x and col y in the table anymore.
+     * This changed with tail call optimization.
+     */
     private TailCall<Long> recursiveGenerateCode(int row, int col, long result, LongUnaryOperator operator) {
         if (row == 1 && col == 1) {
             return TailCalls.done(result);
@@ -17,6 +25,27 @@ public class TailCallRecursionDiagonalCodeGenerator implements DiagonalCodeGener
                     return recursiveGenerateCode(1, row - 1, operator.applyAsLong(result), operator);
                 } else {
                     return recursiveGenerateCode(row + 1, col - 1, operator.applyAsLong(result), operator);
+                }
+            };
+        }
+    }
+
+    /*
+     * This method also uses tail call optimization but it ensures that
+     * the result always corresponds to curRow and curCol.
+     */
+    private TailCall<Long> recursiveGenerateCode2(
+            int row, int col, int curRow, int curCol, long result, LongUnaryOperator operator) {
+        if (row == curRow && col == curCol) {
+            return TailCalls.done(result);
+        } else {
+            return () -> {
+                if (curRow == 1) {
+                    return recursiveGenerateCode2(
+                            row, col, curCol + 1, 1, operator.applyAsLong(result), operator);
+                } else {
+                    return recursiveGenerateCode2(
+                            row, col, curRow - 1, curCol + 1, operator.applyAsLong(result), operator);
                 }
             };
         }
